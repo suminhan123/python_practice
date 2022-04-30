67d9a0264bc18eb64caf923b2744821c1c5aa0c0
이현종 <74046436+LeeHyunJongSNN@users.noreply.github.com>
2022-04-30 10:56:21 +0900

    Add files via upload

---
+++ Vet_for_My_Pet/Vet_for_My_Pet/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vet_for_My_Pet
+{
+    public partial class start_page : Form
+    {
+
+        bool is_move;
+        int val_x, val_y;
+
+        public start_page()
+        {
+            InitializeComponent();
+        }
+
+        private void top_control_mouse_up(object sender, MouseEventArgs e)
+        {
+           is_move = false;
+        }
+
+        private void top_control_mouse_move(object sender, MouseEventArgs e)
+        {
+            if(is_move == true)
+            {
+                this.SetDesktopLocation(MousePosition.X - val_x, MousePosition.Y - val_y);
+            }
+        }
+
+        private void top_control_mouse_down(object sender, MouseEventArgs e)
+        {
+            is_move = true;
+            val_x = e.X;
+            val_y = e.Y;
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            this.Visible = false; 
+            main_page frm = new main_page(); 
+            frm.Owner = this; 
+            frm.Show(); 
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
